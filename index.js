45152b55c0559b44983abc9b207ae750f740b578
Nazar Kruk <nazarkruk@icloud.com>
2022-02-15 16:21:47 -0500

    Need to finish

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+function submitData(name, email){
+    return fetch(
+        'http://localhost:3000/users', {
+            method: 'POST',
+            headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            },
+            body: JSON.stringify({
+              name: `${name}`,
+              email: `${email}`
+            })
+        })
+    .then (res => res.json())
+    .then (data => {
+        let p = document.createElement('p')
+        p.innerText = `${data.id}`
+        document.body.appendChild(p)
+
+
+    })
+    .catch(err=> {
+        let p = document.createElement('p')
+        p.innerText = `${err}`
+        document.body.appendChild(p)
+    })
+}
+
